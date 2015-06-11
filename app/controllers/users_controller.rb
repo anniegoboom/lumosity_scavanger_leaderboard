3bcd91295032989589b3d5bf1db732d761a95981
Annie <annie@lumoslabs.com>
2015-06-11 13:32:37 -0700

    create POST

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    name  = params[:user]
+    score = params[:score]
+    time  = params[:time]
+
+    @user = User.find_by_name(name)
+    if user.present?
+      @user.update(score: score) if user.score > score
+    else
+      @user = User.create!(name: name, score: score, datetime: time)
+    end
+
+    respond_with @user
   end
 end
